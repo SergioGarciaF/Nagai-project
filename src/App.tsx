f83dc33204405f4a9ced3cb25354488732561060
Sergio Garcia Fernandez <sergiogarciafernandez4@gmail.com>
2023-11-23 12:27:48 +0100

    Sing up layout and added buttons

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 import AboutUs from './components/About us/AboutUs'
 import Home from './components/Home/Home'
 import Register from './components/Register-SignUp/Register/Register'
+import Signup from './components/Register-SignUp/Signup/Signup'
 import ProductDetail from './components/Shop/ProductDetail'
 import Shop from './components/Shop/Shop'
 
@@ -13,6 +14,7 @@ function App() {
     <>
       <Home />
       <Register />
+      <Signup />
     </>
   )
 }
--- src/components/Register-SignUp/Signup/Signup.tsx
+++ src/components/Register-SignUp/Signup/Signup.tsx
@@ -1,31 +1,23 @@
 import React from 'react'
-
-import user from '../../../assets/icons/PhUserLight.png'
 import Header from '../../Header/Header'
 import Footer from '../../Footer/Footer'
+import user from '../../../assets/icons/PhUserLight.png'
 
-const Register = () => {
+const Signup = () => {
     return (
         <>
-        <Header />
+            <Header />
             <div className='flex flex-col gap-2 font-inter justify-center mt-20 items-center m-auto'>
                 <img src={user} width={30} alt="" />
-                <div className='flex space-x-2'>
-                    <p className='text-xs'>Client</p>
-                    <input type="checkbox" className="toggle" checked />
-                    <p className='text-xs'>Artist</p>
-                </div>
                 <p className='font-semibold text-sm'>Email:</p>
                 <input type="email" className="input input-bordered w-full max-w-xs" />
-                <p className='font-semibold text-sm'>Username:</p>
-                <input type="text" className="input input-bordered w-full max-w-xs" />
                 <p className='font-semibold text-sm'>Password:</p>
                 <input type="password" className="input input-bordered w-full max-w-xs" />
-                <button className="btn btn-xs btn-outline btn-neutral mt-3">Register</button>
+                <button className="btn btn-xs btn-outline btn-neutral mt-3">Sign Up</button>
             </div>
             <Footer />
         </>
     )
 }
 
-export default Register
+export default Signup
