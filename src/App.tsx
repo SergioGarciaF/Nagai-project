2992bdf525505795ace97785867cbadb68fd85a1
Sergio Garcia Fernandez <sergiogarciafernandez4@gmail.com>
2023-11-23 12:22:42 +0100

    Register layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import './App.css'
 import AboutUs from './components/About us/AboutUs'
 import Home from './components/Home/Home'
+import Register from './components/Register-SignUp/Register/Register'
 import ProductDetail from './components/Shop/ProductDetail'
 import Shop from './components/Shop/Shop'
 
@@ -11,7 +12,7 @@ function App() {
   return (
     <>
       <Home />
-      
+      <Register />
     </>
   )
 }
